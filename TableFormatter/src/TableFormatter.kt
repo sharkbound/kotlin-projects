b227d7a5dbe53c2899f5dc2fa0a33ccede6a6dbc
sharkbound <laptopblaster@gmail.com>
2019-04-05 15:30:40 -0500

    added TableFormatter

---
+++ TableFormatter/src/TableFormatter.kt
@@ -0,0 +1,72 @@
+fun main() {
+    val data = listOf(
+        Row("jimmy"),
+        Row("james", "scientist"),
+        Row("lamb", "baa", "baa", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+    )
+
+    val headers = listOf<String>()
+
+    println(format(listOf("name", "age", "interests"), data))
+}
+
+const val SPACER = "  "
+
+fun format(headers: List<String>, rows: List<Row>): String = buildString {
+    val maxDataLength = maxOf(
+        rows.flatMap { it.data.asIterable() }.map { it.length }.max() ?: return@buildString,
+        headers.map { it.length }.max() ?: 0
+    )
+
+    var index = 0
+    val maxRowLengths = mutableMapOf<Int, Int>()
+    val maxRowCount = rows.asSequence().map { it.data.size }.max() ?: 0
+    val joinedHeaders = buildString {
+        append(SPACER)
+        for ((i, header) in headers.withIndex()) {
+            val maxValueLength = rows.asSequence().map { it.data.getOrElse(i) { "" }.length }.max() ?: 0
+            val maxRowLength = maxOf(header.length, maxValueLength)
+            maxRowLengths[i] = maxRowLength
+            append(header.padEnd(maxRowLength))
+            append(SPACER)
+        }
+    }
+
+    appendln(joinedHeaders)
+    for (row in rows) {
+        append(buildString {
+            append("${index++} ")
+
+            for (i in 0 until maxRowCount) {
+                append(row.data.getOrElse(i) { "" }.padEnd(maxRowLengths[i] ?: maxRowLengths.values.max() ?: 0))
+                append(SPACER)
+            }
+        }.trim())
+        appendln()
+    }
+}
+
+fun String.center(len: Int, padChar: Char = ' '): String {
+    if (length >= len) {
+        return this
+    }
+    val width = len - length
+    val strLen = length
+    val finalLength = strLen + width
+    return buildString(finalLength) {
+        repeat(width / 2) { append(padChar) }
+        append(this@center)
+        while (length < finalLength) {
+            append(padChar)
+        }
+    }
+}
+
+operator fun Char.times(repeatCount: Int): String = buildString {
+    repeat(repeatCount) {
+        append(this@times)
+    }
+}
+
+
+class Row(vararg val data: String)
