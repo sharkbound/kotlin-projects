094a9067a5ce2101eabde524cb9898beaf560861
sharkbound <laptopblaster@gmail.com>
2019-05-05 13:03:53 -0500

    added JFrame.sendCloseEvent() to commonutils

--- CommonUtilities/src/main/kotlin/sharkbound/commonutils/extensions/JFrameExtensions.kt
+++ CommonUtilities/src/main/kotlin/sharkbound/commonutils/extensions/JFrameExtensions.kt
@@ -4,9 +4,20 @@ import sharkbound.commonutils.enums.JFrameCloseOperation
 import java.awt.Container
 import java.awt.Dimension
 import java.awt.Point
+import java.awt.Window
+import java.awt.event.WindowEvent
 import java.lang.IllegalArgumentException
 import javax.swing.JFrame
 
+/**
+ * makes the frame visible
+ *
+ * @param closeOperation the operation to do when the window is closed
+ * @param size the size of the window, cannot be used with [pack]
+ * @param pack if true, the windows is packed to be as small as possible [size]
+ * @param location location to show the window at, cannot be used with [center]
+ * @param center if true, the window will be shown, cannot be used with [location]
+ */
 fun JFrame.showFrame(
     closeOperation: JFrameCloseOperation? = null,
     size: Dimension? = null,
@@ -17,6 +28,10 @@ fun JFrame.showFrame(
 ) {
     if (size != null && pack) {
         throw IllegalArgumentException("cannot do both size and pack, must do or the ether")
+    }
+
+    content?.let {
+        contentPane = content
     }
 
     when {
@@ -35,9 +50,10 @@ fun JFrame.showFrame(
         defaultCloseOperation = it.code
     }
 
-    content?.let {
-        contentPane = content
-    }
-
     isVisible = true
+}
+
+
+fun JFrame.sendCloseEvent(source: Window = this) {
+    dispatchEvent(WindowEvent(source, WindowEvent.WINDOW_CLOSING))
 }
