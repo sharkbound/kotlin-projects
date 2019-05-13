c952940fb433a015265717ce487db85f03629eaf
sharkbound <laptopblaster@gmail.com>
2019-05-13 15:12:13 -0500

    aperently the jar for commonutils did not have suppressException in it

--- CommonUtilities/src/main/kotlin/Main.kt
+++ CommonUtilities/src/main/kotlin/Main.kt
@@ -1,10 +1,5 @@
 @file:JvmName("KotlinCommonUtilTestingMain")
 
-import sharkbound.commonutils.util.suppressException
-import java.lang.IllegalArgumentException
-
 private fun main() {
-    suppressException<IllegalArgumentException> {
-        throw Exception()
-    }
+
 }
--- ExposedDatabaseGUI/src/main/kotlin/sharkbound/ui/ChoicePanel.kt
+++ ExposedDatabaseGUI/src/main/kotlin/sharkbound/ui/ChoicePanel.kt
@@ -1,6 +1,5 @@
 package sharkbound.ui
 
-import sharkbound.commonutils.maybeOf
 import java.awt.BorderLayout
 import java.awt.FlowLayout
 import javax.swing.JButton
