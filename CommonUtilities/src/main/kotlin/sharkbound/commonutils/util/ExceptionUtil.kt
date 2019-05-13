47eee7c73062d680ac5f2b2f8f9e21d791651c65
sharkbound <laptopblaster@gmail.com>
2019-05-12 20:12:34 -0500

    added docs to suppressException function

--- CommonUtilities/src/main/kotlin/sharkbound/commonutils/util/ExceptionUtil.kt
+++ CommonUtilities/src/main/kotlin/sharkbound/commonutils/util/ExceptionUtil.kt
@@ -4,6 +4,8 @@ import java.lang.Exception
 
 /**
  * runs [block] and suppresses [T] if that exception is throw
+ *
+ * @throws Exception if a exception other than [T] is thrown, the thrown exception can be anything other than [T]
  */
 inline fun <reified T : Throwable> suppressException(block: () -> Unit) {
     try {
