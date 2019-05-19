e4fb74718673090e6d1c11329a2b540f0305150c
sharkbound <laptopblaster@gmail.com>
2019-05-18 22:59:23 -0500

    added minimize and maximize button

--- SwingCustomTitleBar/src/main/kotlin/MainWindow.kt
+++ SwingCustomTitleBar/src/main/kotlin/MainWindow.kt
@@ -1,7 +1,6 @@
 import sharkbound.commonutils.enums.JFrameCloseOperation
 import sharkbound.commonutils.extensions.sendCloseEvent
 import sharkbound.commonutils.extensions.showFrame
-import sharkbound.commonutils.util.createGridBagContraint
 import java.awt.*
 import java.awt.event.KeyEvent
 import javax.swing.*
@@ -9,26 +8,38 @@ import javax.swing.border.EmptyBorder
 
 class MainWindow : JFrame("Main Window - SwingCustomTitleBar") {
     val pane = JPanel(GridBagLayout())
+
     val titleBar = JPanel(FlowLayout(FlowLayout.RIGHT)).apply {
         border = EmptyBorder(0, 0, 0, 0)
         background = Color.black
     }
-    val exit = JButton("EXIT").apply {
-        //        addActionListener { sendCloseEvent() }
-        background = Color.gray
-        foreground = Color.white
+
+    val exitButton = JButton("EXIT").apply {
+        addActionListener { sendCloseEvent() }
+        makeDark()
+    }
+
+    val minimizeButton = JButton("minimize").apply {
+        addActionListener { state = Frame.ICONIFIED }
+        makeDark()
+    }
+
+    val maximizeButton = JButton("maximize").apply {
+        addActionListener { extendedState = extendedState or Frame.MAXIMIZED_BOTH }
+        makeDark()
     }
 
     init {
+//       remove the default OS title bar
         isUndecorated = true
-//        setDefaultLookAndFeelDecorated(true)
+        isResizable = true
         layout = BorderLayout()
-        add(titleBar, BorderLayout.NORTH)
+
 
         addEvents()
         addComponents()
-        add(pane, BorderLayout.CENTER)
 
+//        bind escape to close the window
         pane.registerKeyboardAction(
             { sendCloseEvent() },
             KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
@@ -37,20 +48,26 @@ class MainWindow : JFrame("Main Window - SwingCustomTitleBar") {
     }
 
     private fun addEvents() {
-        WindowDragListener(this)
+        TitleBarListener(this)
     }
 
     private fun addComponents() {
         titleBar.apply {
-            add(JButton("QUIT").apply {
-                addActionListener {
-                    sendCloseEvent()
-                }
-            }, createGridBagContraint(0, 0, width = Int.MAX_VALUE))
+            add(minimizeButton)
+            add(maximizeButton)
+            add(exitButton)
         }
+
+        add(titleBar, BorderLayout.NORTH)
+        add(pane, BorderLayout.CENTER)
     }
 
     fun display() {
         showFrame(JFrameCloseOperation.EXIT_ON_CLOSE, size = Dimension(600, 600), center = true)
     }
+}
+
+fun JButton.makeDark() {
+    background = Color.getColor("#2b2b2b")
+    foreground = Color.white
 }
--- SwingCustomTitleBar/src/main/kotlin/TitleBarListener.kt
+++ SwingCustomTitleBar/src/main/kotlin/TitleBarListener.kt
@@ -1,36 +1,34 @@
 import sharkbound.commonutils.extensions.use
-import sharkbound.commonutils.extensions.with
 import java.awt.Point
 import java.awt.event.MouseEvent
 import java.awt.event.MouseListener
 import java.awt.event.MouseMotionListener
 import javax.swing.JFrame
 
-class WindowDragListener(private val frame: JFrame) : MouseListener, MouseMotionListener {
+class TitleBarListener(private val frame: JFrame) : MouseListener, MouseMotionListener {
+    private lateinit var pos: Point
+
     init {
         frame.addMouseMotionListener(this)
         frame.addMouseListener(this)
     }
 
-    private lateinit var pos: Point
     override fun mouseReleased(e: MouseEvent?) {
         pos = Point(0, 0)
     }
 
-    override fun mouseEntered(e: MouseEvent?) {}
-    override fun mouseClicked(e: MouseEvent?) {}
-    override fun mouseExited(e: MouseEvent?) {}
     override fun mousePressed(e: MouseEvent?) {
         pos = e?.point ?: return
     }
 
-    override fun mouseMoved(e: MouseEvent?) {}
     override fun mouseDragged(e: MouseEvent?) {
         e use {
             frame.location = Point(locationOnScreen.x - pos.x, locationOnScreen.y - pos.y)
         }
     }
+
+    override fun mouseEntered(e: MouseEvent?) {}
+    override fun mouseClicked(e: MouseEvent?) {}
+    override fun mouseExited(e: MouseEvent?) {}
+    override fun mouseMoved(e: MouseEvent?) {}
 }
-
-
-val Point.asCoordString get() = "($x, $y)"
