823f9600ff9c7820d0c8414e53d69c30fc22f5c9
sharkbound <laptopblaster@gmail.com>
2019-06-19 13:44:53 -0500

    removed GRID_FILL_BOTH constant

--- SwingDSL/src/main/kotlin/sharkbound/swingdsl/Constants.kt
+++ SwingDSL/src/main/kotlin/sharkbound/swingdsl/Constants.kt
@@ -1,8 +1,5 @@
 package sharkbound.swingdsl
 
-import sharkbound.swingdsl.util.gridBagConstraint
-
-val GRID_FILL_BOTH = gridBagConstraint()
 
 /**
  * helper constant for extension method Container.addAll(NULL to <some jcomponent>)]
