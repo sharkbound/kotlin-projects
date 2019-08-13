8cd15536e6de085cf8faae8032f767830b7495cf
sharkbound <laptopblaster@gmail.com>
2019-08-13 17:22:09 -0500

    added line drawing

---
+++ LineDrawing/src/main/kotlin/main.kt
@@ -0,0 +1,21 @@
+import sharkbound.canvasgame.GameFrame
+import sharkbound.swingdsl.dsl.frame
+import sharkbound.swingdsl.enums.JFrameCloseOperation
+import sharkbound.swingdsl.extensions.add
+import sharkbound.swingdsl.extensions.repaintLoop
+import sharkbound.swingdsl.extensions.showFrame
+import sharkbound.swingdsl.extensions.useGridBagLayout
+import sharkbound.swingdsl.util.gridFillBoth
+import java.awt.Dimension
+
+fun main() {
+    frame {
+        root {
+            useGridBagLayout()
+            add(gridFillBoth()) { GameFrame(this@frame) }
+        }
+        showFrame(JFrameCloseOperation.EXIT_ON_CLOSE, Dimension(600, 600), center = true)
+
+        repaintLoop { 10 }
+    }
+}
