6e40fa1ee497560bcb2e8d3573522cb5580d87af
sharkbound <laptopblaster@gmail.com>
2019-10-08 13:58:57 -0500

    change filepath to filetree compile in build.gradle

--- Misc/src/main/kotlin/sharkbound/PrimaryMain.kt
+++ Misc/src/main/kotlin/sharkbound/PrimaryMain.kt
@@ -1,6 +1,18 @@
 package sharkbound
 
-
 fun main() {
-    
+//    JSONObject(
+//        """
+//        {
+//          "name": {
+//              "last": "james",
+//              "first":"john"
+//          }
+//        }
+//    """.trimIndent()
+//    ).let {
+//        println(it.path<String>("name", "first"))
+//    }
+
 }
+
