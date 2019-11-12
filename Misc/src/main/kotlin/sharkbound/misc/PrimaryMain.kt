628323e2303d0b2049e4d581cb468ae321e53f89
sharkbound <laptopblaster@gmail.com>
2019-11-12 16:17:02 -0600

    ...

--- Misc/src/main/kotlin/sharkbound/misc/PrimaryMain.kt
+++ Misc/src/main/kotlin/sharkbound/misc/PrimaryMain.kt
@@ -2,11 +2,5 @@ package sharkbound.misc
 
 
 fun main() {
-    val odometer = Odometer(FuelGauge(50)).apply {
-        gauge.fill(gauge.capacity)
-        while (nonEmpty) {
-            println("checking the gauge you see its currently at ${check()}")
-        }
-    }
 }
 
