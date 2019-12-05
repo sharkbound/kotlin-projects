e523024b873930da635eaca23ddaeb5b427e273a
sharkbound <laptopblaster@gmail.com>
2019-12-04 18:25:56 -0600

    day1

--- AdventOfCode2019/src/main/kotlin/sharkbound/aoc/day1/Part2.kt
+++ AdventOfCode2019/src/main/kotlin/sharkbound/aoc/day1/Part2.kt
@@ -3,5 +3,14 @@ package sharkbound.aoc.day1
 import sharkbound.aoc.read
 
 fun main() {
-    println(read(1).lines().filter { it.matches("""\d+""".toRegex()) }.map { it.toInt() / 3 - 2 }.sum())
+    println(read(1).lines().mapNotNull { it.toIntOrNull() }.map {
+        var fuel = it
+        var total = 0
+        while (fuel / 3 - 2 > 0) {
+            val newFuel = fuel / 3 - 2
+            total += newFuel
+            fuel = newFuel
+        }
+        total
+    }.sum())
 }
