da36d81ba8153d5bab6ad685521188635f74a1ec
sharkbound <laptopblaster@gmail.com>
2020-03-05 21:30:39 -0600

    jar update

--- CommonUtilities/src/main/kotlin/sharkbound/commonutils/util/RandomUtil.kt
+++ CommonUtilities/src/main/kotlin/sharkbound/commonutils/util/RandomUtil.kt
@@ -20,12 +20,21 @@ fun randInt(min: Int, max: Int): Int = rand.nextInt(min, max + 1)
 fun randBoolean(): Boolean = rand.nextBoolean()
 
 /**
- * gets a random [Double] between [min](inclusive) and [max](inclusive)]
+ * gets a random [Double] between [min] (inclusive) and [max] (inclusive)
  *
  * @return random int between [min](inclusive) and [max](inclusive)]
  * @throws IllegalArgumentException if min is greater than or equal to max
  */
 fun randDouble(min: Double, max: Double): Double = rand.nextDouble(min, max + 1)
+
+
+/**
+ * gets a random [Double] between [min] (inclusive) and [max] (inclusive)
+ *
+ * @return random int between [min](inclusive) and [max](inclusive)]
+ * @throws IllegalArgumentException if min is greater than or equal to max
+ */
+fun randDouble(min: Int, max: Int): Double = randDouble(min.toDouble(), max.toDouble())
 
 
 /**
